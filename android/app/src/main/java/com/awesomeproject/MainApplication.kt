ee739fa38840ad25d323093c2e025eebd649267c
vaidikalaya <vaidikalaya@gmail.com>
2026-01-13 21:37:44 +0530

    first chapter

--- android/app/src/main/java/com/awesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/awesomeproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package com.my_app
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/AwesomeProject/AppDelegate.swift
+++ ios/AwesomeProject/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "AwesomeProject",
+      withModuleName: "my_app",
       in: window,
       launchOptions: launchOptions
     )
